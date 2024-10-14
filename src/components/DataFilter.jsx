1f7239b95ce4f48081f71267021962248628a3b7
Hritik262 <hritikraj121@gmail.com>
2024-10-14 21:45:58 +0530

    Component to select data range

---
+++ src/components/DataFilter.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const DateFilter = ({ onDateChange }) => {
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  const handleDateChange = () => {
+    if (startDate && endDate) {
+      onDateChange(startDate, endDate);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="date"
+        value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}
+      />
+      <input
+        type="date"
+        value={endDate}
+        onChange={(e) => setEndDate(e.target.value)}
+      />
+      <button onClick={handleDateChange}>Filter</button>
+    </div>
+  );
+};
+
+export default DateFilter;
